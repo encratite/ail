e2c102b429607a4ef91590045b54d93662470661
void <git.email.address@googlemail.com>
2009-08-15 17:10:56 +0200

    Added 64-bit preprocessor macros and word types

--- ail/environment.hpp
+++ ail/environment.hpp
@@ -5,4 +5,26 @@
 #define AIL_WINDOWS
 #endif
 
+#elif defined(__i386__) || defined(__alpha__) \
+   || defined(__ia64) || defined(__ia64__) \
+   || defined(_M_IX86) || defined(_M_IA64) \
+   || defined(_M_ALPHA) || defined(__amd64) \
+   || defined(__amd64__) || defined(_M_AMD64) \
+   || defined(__x86_64) || defined(__x86_64__) \
+   || defined(_M_X64) || defined(__bfin__)
+   
+#if \
+	defined(AMD64) || \
+	defined(__ia64) || \
+	defined(__ia64__) || \
+	defined(_M_IA64) || \
+	defined(__amd64) || \
+	defined(__amd64__) || \
+	defined(_M_AMD64) || \
+	defined(__x86_64) || \
+	defined(__x86_64__) || \
+	defined(_M_X64)
+#define AIL_64
+#endif
+
 #endif
--- ail/types.hpp
+++ ail/types.hpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <string>
 #include <set>
+#include <ail/environment.hpp>
+#include <boost/cstdint.hpp>
 
 typedef unsigned char uchar;
 typedef unsigned short ushort;
@@ -12,5 +14,13 @@ typedef unsigned long long ullong;
 
 typedef std::vector<std::string> string_vector;
 typedef std::set<std::string> string_set;
+
+#ifdef AIL_64
+typedef boost::int64_t word;
+typedef boost::uint64_t uword;
+#else
+typedef boost::int32_t word;
+typedef boost::uint32_t uword;
+#endif
 
 #endif
